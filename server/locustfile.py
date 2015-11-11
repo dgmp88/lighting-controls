b1da5d028cab3cb278ac30f9c4c60f3e624926f9
dgmp88 <dgmprichard@gmail.com>
2015-11-11 00:08:02 +0000

    Quick slider with persistent (memory) changes, and a 'has anything changed' request which is very fast

---
+++ server/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpLocust, TaskSet, task
+
+# Run with: locust --host=http://localhost:8888
+
+# We need to respond perhaps 5 times a second, check that's doable.
+
+class UserBehavior(TaskSet):
+    @task
+    def index(self):
+        print self.client.get("/changed/")
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait=99
+    max_wait=100
--- server/run_tornado.py
+++ server/run_tornado.py
@@ -1,14 +1,57 @@
 import tornado.ioloop
 import tornado.web
+import os
+
+changed = 0
+settings = {'brightness': 50}
+
+
+def update_settings(update_dict):
+    global changed
+    for k in update_dict.keys():
+        if settings[k] != update_dict[k]:
+            settings[k] = update_dict[k]
+            changed = 1
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("templates/index.html",
+                    brightness=settings['brightness'])
+
+
+class ChangeValuesHandler(tornado.web.RequestHandler):
+    def post(self):
+        update_dict = {}
+        update_dict['brightness'] = self.get_argument('brightness')
+        update_settings(update_dict)
+        self.redirect("/")
+
+class HasThereBeenAChangeHandler(tornado.web.RequestHandler):
+    def get(self):
+        global changed
+        if changed:
+            self.write("1")
+            changed = 0
+        else:
+            self.write("0")
+
 
 def make_app():
-    return tornado.web.Application([
+    handlers = [
         (r"/", MainHandler),
-    ])
+        (r"/changed/", HasThereBeenAChangeHandler),
+        (r"/dochange/", ChangeValuesHandler),
+    ]
+
+    DEBUG = False
+    if os.environ.get("DEBUG") == "True":
+        DEBUG = True
+
+    settings = {
+        'static_path': os.path.join(os.path.dirname(__file__), 'static')
+    }
+
+    return tornado.web.Application(handlers, debug=DEBUG, **settings)
 
 if __name__ == "__main__":
     app = make_app()
