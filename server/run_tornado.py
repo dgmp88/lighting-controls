96f75d97bb6d396001718dcad44a1453985a2e1b
dgmp88 <dgmprichard@gmail.com>
2016-01-10 21:38:39 +0000

    Working with the arduino method. About to change to pi method.

--- server/run_tornado.py
+++ server/run_tornado.py
@@ -1,45 +1,43 @@
 import tornado.ioloop
 import tornado.web
 import os
+import json
+import requests
 
 changed = 0
-settings = {'brightness': 50}
 
-
-def update_settings(update_dict):
-    global changed
-    for k in update_dict.keys():
-        if settings[k] != update_dict[k]:
-            settings[k] = update_dict[k]
-            changed = 1
+lighting_settings = {'brightness_rgb': [50, 50, 50]}
+ip_settings = json.load(open("secret_stuff.json"))
+arduino_set_rgb_endpoint = ip_settings['arduino_set_rgb']
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("templates/index.html",
-                    brightness=settings['brightness'])
+                    brightness_r=lighting_settings['brightness_rgb'][0],
+                    brightness_g=lighting_settings['brightness_rgb'][1],
+                    brightness_b=lighting_settings['brightness_rgb'][2])
 
 
 class ChangeValuesHandler(tornado.web.RequestHandler):
     def post(self):
-        update_dict = {}
-        update_dict['brightness'] = self.get_argument('brightness')
-        update_settings(update_dict)
+        lighting_settings['brightness_rgb'] = [self.get_argument('brightness_r'),
+                                                self.get_argument('brightness_g'),
+                                                self.get_argument('brightness_b')]
+        for idx, val in enumerate(lighting_settings['brightness_rgb']):
+            lighting_settings['brightness_rgb'][idx] = int(val)
+
+        end_vals = '%03d%03d%03d' % (lighting_settings['brightness_rgb'][0],
+                                     lighting_settings['brightness_rgb'][1],
+                                     lighting_settings['brightness_rgb'][2])
+        set_url = arduino_set_rgb_endpoint + end_vals
+
+        requests.get(set_url)
+
         self.redirect("/")
-
-class HasThereBeenAChangeHandler(tornado.web.RequestHandler):
-    def get(self):
-        global changed
-        if changed:
-            self.write("1")
-            changed = 0
-        else:
-            self.write("0")
-
 
 def make_app():
     handlers = [
         (r"/", MainHandler),
-        (r"/changed/", HasThereBeenAChangeHandler),
         (r"/dochange/", ChangeValuesHandler),
     ]
 
