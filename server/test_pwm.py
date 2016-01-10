d491a15bdc5df11ab51aca7a58b42dce2a219449
geirge <dgmprichard@gmail.com>
2016-01-10 21:47:41 +0000

    Slightly faster easier test

--- server/test_pwm.py
+++ server/test_pwm.py
@@ -12,7 +12,6 @@ red_id = 2
 green_id = 24
 blue_id = 3
 
-
 GPIO.setup(red_id, GPIO.OUT)# set GPIO 25 as output for white led
 GPIO.setup(green_id, GPIO.OUT)# set GPIO 24 as output for red led
 GPIO.setup(blue_id, GPIO.OUT)# set GPIO 24 as output for red led
@@ -21,25 +20,30 @@ red = GPIO.PWM(red_id, 100)      # create object red for PWM on port 24 at 100 H
 green = GPIO.PWM(green_id, 100)      # create object red for PWM on port 24 at 100 Hertz
 blue = GPIO.PWM(blue_id, 100)    # create object white for PWM on port 25 at 100 Hertz
 
-
 blue.start(0)             
 red.start(0)              
 green.start(0)
 
-pause_time = 1.0
+pause_time = 0.333
 
 try:
     while True:
-        blue.ChangeDutyCycle(0)
         red.ChangeDutyCycle(100)
-        #green.ChangeDutyCycle(100)
+        green.ChangeDutyCycle(0)
+        blue.ChangeDutyCycle(0)
 
         sleep(pause_time)
         red.ChangeDutyCycle(0)
-        blue.ChangeDutyCycle(100)
-        green.ChangeDutyCycle(0)
+        green.ChangeDutyCycle(100)
+        blue.ChangeDutyCycle(0)
 
         sleep(pause_time)
+	red.ChangeDutyCycle(0)
+	green.ChangeDutyCycle(0)
+	blue.ChangeDutyCycle(100)
+
+	sleep(pause_time)
+	
 
 except KeyboardInterrupt:
     blue.stop()            # stop the white PWM output
